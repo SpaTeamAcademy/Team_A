b76738a016864a7138dcefa53831e6dc5d6b1a1e
Johnny <johnny.hawranek@gmail.com>
2023-07-20 09:37:55 +0200

    Revert "[003.1] initial commit od user story 003.1"
    This reverts commit d619a2196b6e9f3a1ed6d8c6dc2902304c8a16b2.

---
+++ rock-paper-scissor.js
@@ -0,0 +1,9 @@
+document.getElementById("rockBtn").addEventListener("click", choseOption("rock"));
+document.getElementById("paperBtn").addEventListener("click", choseOption("paper"));
+document.getElementById("scissorBtn").addEventListener("click", choseOption("scissor"));
+
+function choseOption(choice) {
+    let usersChoice = choice;
+    return usersChoice;
+}
+console.log(usersChoice);
