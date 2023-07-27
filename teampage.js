707e84490dc0a7078d2c273ac4c4f4be317c75e4
Io <io.bosse@protonmail.com>
2023-07-27 12:25:02 +0200

    [014] final style changes, added a + or - symbol at the accordion depending on wether or not it is opened
    014 Teampage https://app.asana.com/0/1205072348884926/1205081971096386/f

--- teampage.js
+++ teampage.js
@@ -1,22 +1,27 @@
 let accBtns = document.getElementsByClassName("accBtn");
 let accPanels = document.getElementsByClassName("accPanel");
 let footerLinks = document.getElementsByClassName("footerLink");
+let statuses = document.getElementsByClassName("status");
 
 for (let i=0;i<accBtns.length;i++) {
     accBtns[i].addEventListener("click", function(){
         if (accPanels[i].style.display === "none"){
             accPanels[i].style.display = "block";
+            statuses[i].innerHTML = "-";
         } else {
             accPanels[i].style.display = "none";
+            statuses[i].innerHTML = "+";
         }
     });
 }
 
 for(let i=0;i<footerLinks.length;i++){
     footerLinks[i].addEventListener("click", function(){
-        for(j of accPanels){
-            j.style.display = "none";
+        for(let j=0;j<accPanels.length;j++){
+            accPanels[j].style.display = "none";
+            statuses[j].innerHTML = "+";
         }
+        statuses[i].innerHTML = "-";
         accPanels[i].style.display = "block";
     });
 }
@@ -31,21 +36,26 @@ function accordion(){
     console.log(name);
     if(name === "#io"){
         accPanels[0].style.display = "block";
+        statuses[0].innerHTML = "-";
     }
 
     else if(name === "#erik"){
         accPanels[1].style.display = "block";
+        statuses[1].innerHTML = "-";
     }
 
     else if(name === "#johnny"){
         accPanels[2].style.display = "block";
+        statuses[2].innerHTML = "-";
     }
 
     else if(name === "#florian"){
         accPanels[3].style.display = "block";
+        statuses[3].innerHTML = "-";
     }
 
     else if(name === "#hannah"){
         accPanels[4].style.display = "block";
+        statuses[4].innerHTML = "-";
     }
 }
