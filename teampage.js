bc017a8cd1069afebb420bbd68d0d8c3f2984b5f
Io <io.bosse@protonmail.com>
2023-07-27 10:27:21 +0200

    [014]footer links on the other pages now open the accordions
    014 Teampage https://app.asana.com/0/1205072348884926/1205081971096386/f

--- teampage.js
+++ teampage.js
@@ -10,3 +10,28 @@ for (let i=0;i<accBtns.length;i++) {
         }
     });
 }
+
+window.onload = function(){
+    let url = new URL(window.location.toLocaleString());
+    let name = url.hash;
+    console.log(name);
+    if(name === "#erik"){
+        accPanels[0].style.display = "block";
+    }
+
+    else if(name === "#florian"){
+        accPanels[1].style.display = "block";
+    }
+
+    else if(name === "#hannah"){
+        accPanels[2].style.display = "block";
+    }
+
+    else if(name === "#io"){
+        accPanels[3].style.display = "block";
+    }
+
+    else if(name === "#johnny"){
+        accPanels[4].style.display = "block";
+    }
+}
