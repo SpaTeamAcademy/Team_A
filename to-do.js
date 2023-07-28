11fa3fc832f2c0c08c19c8441c2c9adeb85cfff4
Hannah <ivyxamy.lia@gmail.com>
2023-07-28 11:42:20 +0200

    [006] Johnnys JS file added and combined |
    006 To-Do bzw. Einkaufsliste https://app.asana.com/0/1205072348884926/1205082382297347/f

---
+++ to-do.js
@@ -0,0 +1,20 @@
+
+
+
+
+function addItem() {
+    var newTask = document.getElementById("toDoInput").value;
+    var newItem = document.createElement("li");
+    document.getElementById("invalid-input-warning").innerText = "";
+
+    if (newTask === "") {
+        document.getElementById("invalid-input-warning").innerText = "invalid input";
+    } else {
+        newItem.innerHTML = newTask;
+        document.getElementById("toDo-ul").appendChild(newItem);
+    }
+
+    document.getElementById("toDoInput").value = ""
+}
+
+
