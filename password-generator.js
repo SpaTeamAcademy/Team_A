f0695d0b66b747d54d128ac2ecc3dc8b6de28a12
Io <io.bosse@protonmail.com>
2023-07-31 10:48:30 +0200

    [018] added a function to check for correct input values
    018 Passwort-Generator (Längen Auswahl) https://app.asana.com/0/1205072348884926/1205158227496893/f

--- password-generator.js
+++ password-generator.js
@@ -3,11 +3,33 @@ let RandomLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"
 
 
 function GenerateButton(){
-    let length = document.getElementById("length").value;
+    let length = checkInputValue();
     let Password ="";
+
     for(let x = length; x > 0; x--){
         let RandNumber = Math.floor(Math.random()*26);
         Password = Password + RandomLetters[RandNumber]; 
     }
     document.getElementById("PasswordDisplay").innerHTML = Password;
+}
+
+function checkInputValue(){
+    let input = document.getElementById("length").value;
+
+    if(input === ""){
+        input = 8;
+        document.getElementById("length").value = 8;
+    }
+
+    else if(input < 4){
+        input = 4;
+        document.getElementById("length").value = 4;
+    }
+
+    else if(input > 50){
+        input = 50;
+        document.getElementById("length").value = 50;
+    }
+
+    return input;
 }
