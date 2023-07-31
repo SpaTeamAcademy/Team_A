6757036ec5e10f2d83e654dcdb770d2ba753187f
Johnny <johnny.hawranek@gmail.com>
2023-07-31 09:55:16 +0200

    [006] added delete button after new list entry

--- to-do.js
+++ to-do.js
@@ -5,6 +5,7 @@
 function addItem() {
     var newTask = document.getElementById("toDoInput").value;
     var newItem = document.createElement("li");
+    
     document.getElementById("invalid-input-warning").innerText = "";
 
     if (newTask === "") {
@@ -12,9 +13,14 @@ function addItem() {
     } else {
         newItem.innerText = newTask;
         document.getElementById("toDo-ul").appendChild(newItem);
+        var deleteButton = document.createElement("button");
+        deleteButton.innerText = "X";
+        document.getElementById("toDo-ul").appendChild(deleteButton);
     }
 
     document.getElementById("toDoInput").value = ""
 }
 
+function deleteItem(){
 
+}
