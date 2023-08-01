fa750353c5fe050f45ff5ea417ca3ded84d6984a
Florian <cb10@specialisterne.at>
2023-08-01 12:13:51 +0200

    added big letters, special letters and numbers to the password and selectable with checkboxes

--- password-generator.js
+++ password-generator.js
@@ -1,14 +1,19 @@
 //  document.getElementById("GeneratePasswordLength").value;   does nothing yet beside exist on html
 let RandomLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
+let RandomLettersGross = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let RandomLettersSonder = ["!","#","$","%","(",")","*","+",",","-",".","/",":",";","=","?","@","[","]","^","_","{","|","}","~"];
+let RandomZahlen = ["0","1","2","3","4","5","6","7","8","9"];
+let Random = [];
 
 function GenerateButton(){
     let length = checkInputValue();
     let Password ="";
-
+    let Random = MainRando();
+    console.log(document.getElementById("klein").value);
+    
     for(let x = length; x > 0; x--){
-        let RandNumber = Math.floor(Math.random()*26);
-        Password = Password + RandomLetters[RandNumber]; 
+        let RandNumber = Math.floor(Math.random()*Random.length);
+        Password = Password + Random[RandNumber]; 
     }
     document.getElementById("PasswordDisplay").innerHTML = Password;
 }
@@ -38,3 +43,32 @@ function checkInputValue(){
 
     return input;
 }
+
+
+function MainRando(){
+    Random=[];
+    if(document.getElementById("klein").checked){
+        for(let x=0;x<RandomLetters.length;x++){
+        Random.push(RandomLetters[x]);
+        }
+    }
+
+    if(document.getElementById("gross").checked){
+        for(let x=0;x<RandomLettersGross.length;x++){
+        Random.push(RandomLettersGross[x]);
+        }
+    }    
+
+    if(document.getElementById("sonder").checked){
+        for(let x=0;x<RandomLettersSonder.length;x++){
+        Random.push(RandomLettersSonder[x]);
+        }
+    }    
+
+    if(document.getElementById("zahl").checked){
+        for(let x=0;x<RandomZahlen.length;x++){
+        Random.push(RandomZahlen[x]);
+        }
+    }    
+        return Random;
+    }
