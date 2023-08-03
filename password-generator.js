c51985d7effa48c4b10e6071900bdbc14da9d800
Florian <cb10@specialisterne.at>
2023-08-03 12:22:18 +0200

    renamed stuff be only english instead of a mix

--- password-generator.js
+++ password-generator.js
@@ -1,7 +1,7 @@
-let RandomLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-let RandomLettersGross = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let RandomLettersSmall = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let RandomLettersBig = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 let RandomLettersSonder = ["!","#","$","%","(",")","*","+",",","-",".","/",":",";","=","?","@","[","]","^","_","{","|","}","~"];
-let RandomZahlen = ["0","1","2","3","4","5","6","7","8","9"];
+let RandomNumbers = ["0","1","2","3","4","5","6","7","8","9"];
 let Random = [];
 document.getElementById("small").checked = true;
 
@@ -16,10 +16,10 @@ function GenerateButton(){
     }
     //Add RandomLetter/Gross/Sonder/Zahlen to Password here amount of words reduced by length-Checkall
     Password = Password.split();  //changes Password to Array
-    if(document.getElementById("small").checked){Password.splice(Math.floor(Math.random()*Password.length),0,RandomLetters[Math.floor(Math.random()*RandomLetters.length)])}
-    if(document.getElementById("big").checked){Password.splice(Math.floor(Math.random()*Password.length),0,RandomLettersGross[Math.floor(Math.random()*RandomLettersGross.length)])}
+    if(document.getElementById("small").checked){Password.splice(Math.floor(Math.random()*Password.length),0,RandomLettersSmall[Math.floor(Math.random()*RandomLettersSmall.length)])}
+    if(document.getElementById("big").checked){Password.splice(Math.floor(Math.random()*Password.length),0,RandomLettersBig[Math.floor(Math.random()*RandomLettersBig.length)])}
     if(document.getElementById("sonder").checked){Password.splice(Math.floor(Math.random()*Password.length),0,RandomLettersSonder[Math.floor(Math.random()*RandomLettersSonder.length)])}
-    if(document.getElementById("number").checked){Password.splice(Math.floor(Math.random()*Password.length),0,RandomZahlen[Math.floor(Math.random()*RandomZahlen.length)])}
+    if(document.getElementById("number").checked){Password.splice(Math.floor(Math.random()*Password.length),0,RandomNumbers[Math.floor(Math.random()*RandomNumbers.length)])}
     document.getElementById("PasswordDisplay").innerHTML = Password.join("");
 }
 
@@ -53,14 +53,14 @@ function checkInputValue(){
 function MainRando(){
     Random=[];
     if(document.getElementById("small").checked){
-        for(let x=0;x<RandomLetters.length;x++){
-        Random.push(RandomLetters[x]);
+        for(let x=0;x<RandomLettersSmall.length;x++){
+        Random.push(RandomLettersSmall[x]);
         }
     }
 
     if(document.getElementById("big").checked){
-        for(let x=0;x<RandomLettersGross.length;x++){
-        Random.push(RandomLettersGross[x]);
+        for(let x=0;x<RandomLettersBig.length;x++){
+        Random.push(RandomLettersBig[x]);
         }
     }    
 
@@ -71,8 +71,8 @@ function MainRando(){
     }    
 
     if(document.getElementById("number").checked){
-        for(let x=0;x<RandomZahlen.length;x++){
-        Random.push(RandomZahlen[x]);
+        for(let x=0;x<RandomNumbers.length;x++){
+        Random.push(RandomNumbers[x]);
         }
     }    
         return Random;
